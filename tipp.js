6e11c211973aacfebaa3a1693180ad28edfb81dc
pr2bm <148187114+pr2bm-danial@users.noreply.github.com>
2024-07-02 00:16:01 +0200

    Add files via upload

--- tipp.js
+++ tipp.js
@@ -1,27 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const token = localStorage.getItem('token');
+            const token = localStorage.getItem('token');
 
-    if (!token) {
-        window.location.href = 'https://pr2bm-danial.github.io/login/login.html';
-    } else {
-        fetch('http://localhost:3000/protected', {
-            method: 'GET',
-            headers: {
-                'Authorization': token
+            if (!token) {
+                window.location.href = 'https://pr2bm-danial.github.io/login/login.html';
+            } else {
+                fetch('http://localhost:3000/protected', {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Token ungültig');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    console.log('Zugriff gewährt:', data.message);
+                })
+                .catch(error => {
+                    console.error('Fehler:', error);
+                    window.location.href = 'https://pr2bm-danial.github.io/login/login.html';
+                });
             }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Token ungültig');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Zugriff gewährt:', data.message);
-        })
-        .catch(error => {
-            console.error('Fehler:', error);
-            window.location.href = 'https://pr2bm-danial.github.io/login/login.html';
         });
-    }
-});
